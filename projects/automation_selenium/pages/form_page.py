b2a1542a3937f2e34375f90c13241e60cd297522
Dima Tkachuk <dimatkachuk2607@gmail.com>
2025-11-24 11:55:52 +0200

    No file update, testing github actions workflow

--- projects/automation_selenium/pages/form_page.py
+++ projects/automation_selenium/pages/form_page.py
@@ -1,6 +1,6 @@
 """
 Page Object for automating the form page
-defines all the locators and methods to use them through test
+defines all the locators and methods to use them through tests
 """
 
 from selenium.webdriver.common.by import By
